b545d6b6cde06d20305e22b8edce4c5a85e82bf3
BuyannemekhM <144758466+BuyannemekhMunkh@users.noreply.github.com>
2023-09-13 20:23:58 +0800

    Update FirstProgramToGit.py
    cheese jokes

--- FirstProgramToGit.py
+++ FirstProgramToGit.py
@@ -1,4 +1,12 @@
 gr='Hello World'
 print(gr)
 n=str(input("What's your name? \nMy name is:"))
-print('Hello',n+',','nice to meet you.')
+r=randint(0,4)
+if n=='Chedda':
+  n='Mister '+n
+  c='Goulda'
+elif r==3:
+  c='Cheesed'
+else:
+  c='nice'
+print('Hello',n+',',c,'to meet you.')
